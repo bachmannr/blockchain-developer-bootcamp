667f16b2d925b7230af8e587ad9205abea166b99
Ralph <ralph@DESKTOP-1UIL6EO.localdomain>
2024-05-09 08:47:45 -0500

    back up 050924

---
+++ contracts/Token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    // State variable to store the greeting
+    string public name = "Ralph Token";
+    // Constructor to set the initial greeting
+    //constructor(string memory _greeting) {
+    //    greeting = _greeting;
+    //}
+    //function getGreeting() public view returns (string memory) {
+    //    return greeting;
+    //}
+    //function setGreeting(string memory _greeting) public {
+    //    greeting = _greeting;
+    //}
+}
---
+++ scripts/1.deploy_token.js
@@ -0,0 +1,24 @@
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+  // We get the contract to deploy / await hre.ethers.getContractFactory("Token")
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log('Token deployed to: ${token.address}'); // address not given
+}
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+    
+  })
